999250545bad2ac3ba58d9f536aabd7c1fce1a9b
Anthony <140103244+AnthonyV-ASC2023@users.noreply.github.com>
2023-08-01 14:14:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const submitButton = document.getElementById("submit_button");
+  submitButton.addEventListener("click", handleSubmit);
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const titleInput = document.getElementById("title_input");
+  const nounInput = document.getElementById("noun");
+  const verbInput = document.getElementById("verb");
+  const adjectiveInput = document.getElementById("adjective");
+
+  const title = titleInput.value.trim();
+  const noun = nounInput.value.trim();
+  const verb = verbInput.value.trim();
+  const adjective = adjectiveInput.value.trim();
+
+  if (title === "" || noun === "" || verb === "" || adjective === "") {
+    alert("Please fill in all fields");
+    return;
+  }
+
+  const storyResult = document.getElementById("story_result");
+  const adlibText = `Last night I ate a ${noun}, and today I just had to ${verb}. What a ${adjective} day!`;
+  storyResult.textContent = adlibText;
+
+  const formContainer = document.querySelector(".form_container");
+  formContainer.style.display = "none";
+}
